7e8eb5ffc10636ac30b74cd441a5d2cd3ed66f47
guilhermezanollo <guilherme.zanollo@gmail.com>
2025-02-05 11:37:46 -0300

    Primeira atualização do curso de Lógica em Csharp

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Exercicio 1
+            Console.WriteLine("Entre com seu nome completo:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int numeroQuartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double precoProduto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe seu último nome, idade e altura (mesma linha)");
+            string[] vet = Console.ReadLine().Split(' ');
+            string lastName = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(name);
+            Console.WriteLine(numeroQuartos);
+            Console.WriteLine(precoProduto.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+            */
+
+            /*
+            // Funções Matemáticas - sqrt: raiz quadrada - pow: potenciação - abs: valor absoluto
+            double x = 3.0;
+            double y = 4.0;
+            double z = -5.0;
+            double A, B, C;
+
+            A = Math.Sqrt(x); // A recebendo x, raiz quadrada de x = 3.0
+            B = Math.Sqrt(y);
+            C = Math.Sqrt(25.0);
+            Console.WriteLine("Raiz quadrada de " + x + " = " + A);
+            Console.WriteLine("Raiz quadrada de " + y + " = " + B);
+            Console.WriteLine("Raiz quadrada de 25 = " + C);
+
+            A = Math.Pow(x, y);
+            B = Math.Pow(x, 2.0);
+            C = Math.Pow(5.0, 2.0);
+            Console.WriteLine(x + " elevado a " + y + " = " + A);
+            Console.WriteLine(x + " elevado ao quadrado = " + B);
+            Console.WriteLine("5 elevado ao quadrado = " + C);
+
+            A = Math.Abs(y); // A recebe Abs de y
+            B = Math.Abs(z);
+            Console.WriteLine("Valor absoluto de " + y + " = " + A);
+            Console.WriteLine("Valor absoluto de " + z + " = " + A);
+
+            Console.ReadLine();
+            */
+
+
+        }
+    }
+}
